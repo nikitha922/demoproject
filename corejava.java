9ff498d2229d898ccc30140a3c8383f36f7a013a
nikitha922 <nikithamadishetty990@gmail.com>
2023-03-14 00:23:13 -0700

    core java

---
+++ corejava.java
@@ -0,0 +1,22 @@
+package demoproject;
+
+public class corejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        int myNum = 5; 
+        String websitePage = "amazon.com";
+        char letter = 'n';
+        double dec = 4.99;
+        boolean myCard = true;
+        
+        System.out.println(dec+"this value is true");
+        System.out.println(websitePage+dec);
+        
+        
+   
+
+	}
+
+}
